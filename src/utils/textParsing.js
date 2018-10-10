6cec5b7d4d592848e3f26e50482f0f65987b2e02
Tonia Blair <tonia@Jamess-MacBook-Pro.local>
2018-10-10 13:32:11 -0400

    Modify textParsing

--- src/utils/textParsing.js
+++ src/utils/textParsing.js
@@ -1,25 +1,31 @@
-export const joinArrayGrammatically = (arr) => {
+export const joinArrayGrammatically = (arrayOfWords) => {
+  const arr = arrayOfWords;
+
   let outStr = '';
-    if (arr.length === 1) {
-        outStr = arr[0];
+
+  if (arr.length === 1) {
+      outStr = arr[0];
     } else if (arr.length === 2) {
         outStr = arr.join(' and ');
     } else if (arr.length > 2) {
-        outStr = arr.slice(0, -1).join(', ') + ', and ' + arr.slice(-1);
-    }
-    return outStr;
+      outStr = arr.slice(0, -1).join(', ') + ', and ' + arr.slice(-1);
+  }
+  return outStr;
 };
 
 export const capitalizeWord = (word) => {
-  let lower = word.toLowerCase();
+  const uncapitalized = word;
+
+  const lower = uncapitalized.toLowerCase();
 
   return lower.charAt(0).toUpperCase() + lower.substr(1);
 };
 
 export const initialCasePhrase = (phrase) => {
-  let words = [];
+  const nonInitialCased = phrase;
 
-  let substrings = phrase.split(' ');
+  let words = [];
+  const substrings = nonInitialCased.split(' ');
 
   substrings.forEach(s => words.push(capitalizeWord(s)));
 
